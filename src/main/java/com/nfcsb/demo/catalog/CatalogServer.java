2b3f8d48f0dab8cd27ca42d05ccdc9a063c3ac65
Zlatko Cajic <zlatkoc@gmail.com>
2017-03-22 14:52:32 +0100

    code formatted

--- src/main/java/com/nfcsb/demo/catalog/CatalogServer.java
+++ src/main/java/com/nfcsb/demo/catalog/CatalogServer.java
@@ -13,7 +13,9 @@ public class CatalogServer {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value = "/greeting/{lname}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/greeting/{lname}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Greeting greeting(
             @RequestParam(value="fname", defaultValue="World") String fname,
             @PathVariable(value="lname") String lname) {
