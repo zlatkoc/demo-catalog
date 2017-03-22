7f185983c65b27d23715cb43a42b48c56bd874bc
Zlatko Cajic <zlatkoc@gmail.com>
2017-03-22 15:53:30 +0100

    an example exception implemented + logging added

--- src/main/java/com/nfcsb/demo/catalog/CatalogServer.java
+++ src/main/java/com/nfcsb/demo/catalog/CatalogServer.java
@@ -2,8 +2,12 @@ package com.nfcsb.demo.catalog;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @RestController
@@ -11,13 +15,23 @@ public class CatalogServer {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private static final Logger logger = LoggerFactory.getLogger(CatalogServer.class);
 
     @RequestMapping(value = "/greeting/{lname}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Greeting greeting(
             @RequestParam(value="fname", defaultValue="World") String fname,
-            @PathVariable(value="lname") String lname) {
+            @PathVariable(value="lname") String lname,
+            HttpServletRequest request)
+    {
+        logger.info("request url = {}", request.getRequestURI());
+
+        if (lname.equals("twitter")) {
+            logger.error("invalid lname = {}", lname);
+            throw new BadRequestException("invalid lname: " + lname);
+        }
+
         String msg = String.format(template, counter.getAndIncrement());
         return new Greeting(fname, lname, msg);
     }
