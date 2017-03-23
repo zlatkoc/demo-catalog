9f1706ea4565f58a86bf3301ca3fa8cb35a9af81
Zlatko Cajic <zlatkoc@gmail.com>
2017-03-23 12:34:39 +0100

    swagger config changed

--- src/main/java/com/nfcsb/demo/catalog/SwaggerConfig.java
+++ src/main/java/com/nfcsb/demo/catalog/SwaggerConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,9 +16,28 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.nfcsb.demo.catalog"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "My REST API",
+                "Some custom description of API.",
+                "API TOS",
+                "Terms of service",
+                contact(),
+                "License of API",
+                "API license URL");
+        return apiInfo;
+    }
+
+    private Contact contact() {
+        Contact contact = new Contact("zc", "http://www.google.com", "zc@zc.com");
+        return contact;
     }
 }
