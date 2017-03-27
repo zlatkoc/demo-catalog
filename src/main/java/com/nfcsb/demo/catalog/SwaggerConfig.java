060ee8696f3e99911b443d47daae875e712c9c4a
Zlatko Cajic <zlatkoc@gmail.com>
2017-03-27 12:09:13 +0200

    added x-token api key auth to swagger ui

--- src/main/java/com/nfcsb/demo/catalog/SwaggerConfig.java
+++ src/main/java/com/nfcsb/demo/catalog/SwaggerConfig.java
@@ -2,21 +2,30 @@ package com.nfcsb.demo.catalog;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
+        ArrayList<Parameter> globalOperationParameters = new ArrayList<>();
+        ArrayList<SecurityScheme> securitySchemes = new ArrayList<>();
+        securitySchemes.add(new ApiKey("X-Token", "X-Token", "header"));
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .useDefaultResponseMessages(false)
+ //               .globalOperationParameters(globalOperationParameters)
+                .securitySchemes(securitySchemes)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.nfcsb.demo.catalog"))
                 .paths(PathSelectors.any())
